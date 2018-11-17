018c0b322754fc8146baaa4ac2ced69c44b03957
Adriaan van Rossum <git@adriaan.io>
2018-11-17 05:44:59 +0200

    Add create list

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,13 @@ const { generateHTML } = require.main.require('./services/utils')
 
 module.exports = {
   get: async (req, res) => {
-
     const { email } = req.user
     const template = generateHTML({ email })
 
     const html = `
-      <form method="post">
-        <h2 class="mb-4">Signup</h2>
-        <div class="form-group">
-          <label for="nameField">Full name</label>
-          <input type="text" class="form-control" id="nameField" placeholder="Enter full name...">
-          <small class="form-text text-muted">Please use your real name because of trust.</small>
-        </div>
-        <div class="form-group">
-          <label for="emailField">Email address</label>
-          <input type="email" class="form-control" id="emailField" placeholder="Enter email address...">
-          <small class="form-text text-muted">You will receive a magic link by email to login.</small>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+      <p>Here I will create a list</p>
     `
 
-    return end(req, res, 200, template)
+    return end(req, res, 200, template + html)
   }
 }
