6f358a105a86c2a2bb36a61b19bf0287223eaa6c
Adriaan van Rossum <git@adriaan.io>
2018-11-25 20:43:53 +0200

    Fix dimensions of cover image

--- routes/about.js
+++ routes/about.js
@@ -22,7 +22,7 @@ module.exports = {
         <p>There are a lot of lists out there with privacy first products. But those lists are always a one way streat. You have to trust the source and only get one side of the story. What we are creating is a list of products where you can publicly see what other people are saying about it. We also add features to help upcoming privacy loyal businesses so they can share and communicate about their product.</p>
 
         <h5 class="mt-5">Can I add my project?</h5>
-        <p>Contact us to get yourself listed via <a href="mailto:adriaan@privacyfirstproducts.com">adriaan@privacyfirstproducts.com</a>. Make sure to attach an image of your logo (700 x 350px) which should be on a non-white background. Be sure to have registered on the platform with as many founder/creators of your product and link them in the email. Write a non-marketing description so we can use that on our website. All information provided we can change to match our quality standards.</p>
+        <p>Contact us to get yourself listed via <a href="mailto:adriaan@privacyfirstproducts.com">adriaan@privacyfirstproducts.com</a>. Make sure to attach an image of your logo (700 x 450px) which should be on a non-white background. Be sure to have registered on the platform with as many founder/creators of your product and link them in the email. Write a non-marketing description so we can use that on our website. All information provided we can change to match our quality standards.</p>
       </div>
     </div>
     `
